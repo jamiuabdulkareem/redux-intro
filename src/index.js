1e9213e9cb1930b427d4e7c78e6de0cc89e42380
Jamiu Abidemi Abdulkareem <106557387+jamiuabdulkareem@users.noreply.github.com>
2023-10-14 00:25:22 +0100

    update

--- src/index.js
+++ src/index.js
@@ -6,13 +6,10 @@ import App from "./App";
 
 import store from "./store";
 
-store.dispatch({ type: "account/deposit", payload: 250 });
-console.log(store.getState());
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
