0682e41caadd14bcf731c8289bc786584d82d113
Jamiu Abidemi Abdulkareem <106557387+jamiuabdulkareem@users.noreply.github.com>
2023-10-18 02:02:14 +0100

    update

--- src/features/accounts/AccountOperations.js
+++ src/features/accounts/AccountOperations.js
@@ -23,7 +23,7 @@ function AccountOperations() {
 
     dispatch(deposit(depositAmount, currency));
     setDepositAmount("");
-    setCurrency("");
+    setCurrency("USD");
   }
 
   function handleWithdrawal() {
