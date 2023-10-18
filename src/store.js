b513d6f7d4d61cf1c270cf2d1fd1ee47c7756d6f
Jamiu Abidemi Abdulkareem <106557387+jamiuabdulkareem@users.noreply.github.com>
2023-10-18 02:07:15 +0100

    update

--- src/store.js
+++ src/store.js
@@ -1,17 +1,13 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/cutomerSlice";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
 
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
 });
-
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
 
 export default store;
